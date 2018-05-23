57ce83a33a0127e02b69dd47e7508b8bff82b161
Girish Korde <gkorde@shutterfly.com>
2018-05-23 15:36:04 +0530

    js changes

--- app.js
+++ app.js
@@ -1,214 +1,39 @@
-angular.module('app', ['dynform'])
-  .controller('AppCtrl', ['$scope', function ($scope) {
-    $scope.stdFormTemplate = {
-      "fieldset": {
-        "type": "fieldset",
-        "label": "fieldset",
-        "fields": {
-          "text": {
-            "type": "text",
-            "label": "text",
-            "placeholder": "text"
-          },
-          "date": {
-            "type": "date",
-            "label": "date",
-            "placeholder": "date"
-          },
-          "datetime": {
-            "type": "datetime",
-            "label": "datetime",
-            "placeholder": "datetime"
-          },
-          "datetime-local": {
-            "type": "datetime-local",
-            "label": "datetime-local",
-            "placeholder": "datetime-local"
-          },
-          "email": {
-            "type": "email",
-            "label": "email",
-            "placeholder": "email"
-          },
-          "month": {
-            "type": "month",
-            "label": "month",
-            "placeholder": "month"
-          },
-          "coordinates-fieldset": {
-            "type": "fieldset",
-            "label": "nested model example",
-            "fields": {
-              "coordinates.lat": {
-                "type": "number",
-                "label": "coordinates.lat",
-                "placeholder": "coordinates.lat",
-                "val": 36.5
-              },
-              "coordinates.lon": {
-                "type": "number",
-                "label": "coordinates.lon",
-                "placeholder": "coordinates.lon",
-                "val": -0.15
-              }
-            }
-          },
-          "number": {
-            "type": "number",
-            "label": "number",
-            "placeholder": "number"
-          },
-          "password": {
-            "type": "password",
-            "label": "password",
-            "placeholder": "password"
-          },
-          "search": {
-            "type": "search",
-            "label": "search",
-            "placeholder": "search"
-          },
-          "tel": {
-            "type": "tel",
-            "label": "tel",
-            "placeholder": "tel"
-          },
-          "textarea": {
-            "type": "textarea",
-            "label": "textarea",
-            "placeholder": "textarea",
-            "splitBy": "\n",
-            "val": ["This array should be","separated by new lines"]
-          },
-          "time": {
-            "type": "time",
-            "label": "time",
-            "placeholder": "time"
-          },
-          "url": {
-            "type": "url",
-            "label": "url",
-            "placeholder": "url"
-          },
-          "week": {
-            "type": "week",
-            "label": "week",
-            "placeholder": "week"
-          }
-        }
-      },
-      "checkbox": {
-        "type": "checkbox",
-        "label": "checkbox"
-      },
-      "color": {
-        "type": "color",
-        "label": "color"
-      },
-      "file": {
-        "type": "file",
-        "label": "file",
-        "multiple": true
-      },
-      "range": {
-        "type": "range",
-        "label": "range",
-        "model": "number",
-        "val": 42,
-        "minValue": -42,
-        "maxValue": 84
-      },
-      "select": {
-        "type": "select",
-        "label": "select",
-        "empty": "nothing selected",
-        "options": {
-          "first": {
-            "label": "first option"
-          },
-          "second": {
-            "label": "second option",
-            "group": "first group"
-          },
-          "third": {
-            "label": "third option",
-            "group": "second group"
-          },
-          "fourth": {
-            "label": "fourth option",
-            "group": "first group"
-          },
-          "fifth": {
-            "label": "fifth option"
-          },
-          "sixth": {
-            "label": "sixth option",
-            "group": "second group"
-          },
-          "seventh": {
-            "label": "seventh option"
-          },
-          "eighth": {
-            "label": "eighth option",
-            "group": "first group"
-          },
-          "ninth": {
-            "label": "ninth option",
-            "group": "second group"
-          },
-          "tenth": {
-            "label": "tenth option"
-          }
-        }
-      },
-     
-      "radio": {
-        "type": "radio",
-        "label": "radio",
-        "values": {
-          "first": "first option",
-          "second": "second option",
-          "third": "third option",
-          "fourth": "fourth option",
-          "fifth": "fifth option"
-        }
-      },
-      "button": {
-        "type": "button",
-        "label": "button"
-      },
-      "hidden": {
-        "type": "hidden",
-        "label": "hidden",
-        "val": "hidden"
-      },
-    
-      "legend": {
-        "type": "legend",
-        "label": "legend"
-      },
-      "reset": {
-        "type": "reset",
-        "label": "reset"
-      },
-      "submit": {
-        "type": "submit",
-        "label": "submit"
-      }
-    };
-    $scope.stdFormData = {};
-    $scope.urlFormData = {};
-  }])
-  .filter('pretty', function() {
-    return function (input) {
-      var temp;
-      try {
-        temp = angular.fromJson(input);
-      }
-      catch (e) {
-        temp = input;
-      }
-      
-      return angular.toJson(temp, true);
-    };
-  });
+
+ 
+ var app = angular.module("dynamicFieldsPlugin", []);
+ 
+ app.controller("dynamicFields", function($scope) {
+   
+   $scope.choices = [{id: 'choice1', name: 'choice1'}, {id: 'choice2', name: 'choice2'}, {id: 'choice3', name: 'choice3'}];
+   $scope.boxes = [{id: 'box1', name: 'box1'}, {id: 'box2', name: 'box2'}, {id: 'box3', name: 'box3'}];
+   
+   $scope.addNewChoice = function() {
+     var newItemNo = $scope.choices.length+1;
+     $scope.choices.push({'id' : 'choice' + newItemNo, 'name' : 'choice' + newItemNo});
+   };
+   
+   $scope.removeNewChoice = function() {
+     var newItemNo = $scope.choices.length-1;
+     if ( newItemNo !== 0 ) {
+      $scope.choices.pop();
+     }
+   };
+   
+   $scope.showAddChoice = function(choice) {
+     return choice.id === $scope.choices[$scope.choices.length-1].id;
+   };
+   $scope.showAddbox = function(box) {
+     return box.id === $scope.boxes[$scope.boxes.length-1].id;
+   };
+   $scope.addNewBox = function() {
+     var newItemNo = $scope.boxes.length+1;
+     $scope.boxes.push({'id' : 'box' + newItemNo, 'name' : 'box' + newItemNo});
+   };
+   $scope.removeNewBox = function() {
+     var newItemNo = $scope.boxes.length-1;
+     if ( newItemNo !== 0 ) {
+      $scope.boxes.pop();
+     }
+   };
+   
+ });
