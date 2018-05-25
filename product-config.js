d565193e28f932618c8de3341d6ac6904b9f813d
Girish Korde <gkorde@shutterfly.com>
2018-05-25 13:16:29 +0530

    js and css changes

--- product-config.js
+++ product-config.js
@@ -1,24 +1,14 @@
 var templateFields = [{
         "type": "text",
-        "label": "First Name",
+        "label": "Company Name",
         "model": "name.first",
         "name": "Company_Name"
     },
-    {
-        "type": "text",
-        "label": "Last Name",
-        "model": "name.last"
-        
-    },
-    {
-        "type": "email",
-        "label": "Email Address",
-        "model": "email"
-    },
+    
     {
     "type": "select",
     "model": "select",
-    "label": "select",
+    "label": "Funding Type",
     "name": "Funding_Type",
     "empty": "nothing selected",
     "options": {
@@ -29,8 +19,66 @@ var templateFields = [{
         "label": "Fully Insured",
         "group": "first group"
       }
+   }
+  },
+   {
+    "type": "select",
+    "model": "select",
+    "label": "Is your customer located in one or both of the following states?",
+    "name": "Funding_Type_Follow_Up",
+    "empty": "nothing selected",
+    "options": {
+      "Neither": {
+        "label": "Neither"
+      },
+      "New York": {
+        "label": "New York"
+      },
+      "Illinois": {
+        "label": "Illinois"
+      },
+      "Both": {
+        "label": "Both"
+      }
+   }
+  },
+   {
+    "type": "select",
+    "model": "select",
+    "label": "Line of Buisness",
+    "name": "line_of_buisness",
+    "empty": "nothing selected",
+    "options": {
+      "National Accounts": {
+        "label": "National Accounts"
+      },
+      "Key Accounts": {
+        "label": "Key Accounts"
+      },
+      "Small Buisness": {
+        "label": "Small Buisness"
+      },
+      "Public Sector": {
+        "label": "Public Sector"
+      }
+   }
+  },
+   {
+    "type": "select",
+    "model": "select",
+    "label": "Audience",
+    "name": "Audience",
+    "empty": "nothing selected",
+    "options": {
+      "Non-Union": {
+        "label": "Non-Union"
+      },
+      "Union": {
+        "label": "Union"
+      }
+   }
   }
-}];
+];
 
 $j(document).ready(function() {
  	var scope = angular.element(document.getElementById('buildTemplateID')).scope().initialize('tdForm');
